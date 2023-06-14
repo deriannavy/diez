a2e767fdb4cb0785729cf2fea1fa3096d24cdb0a
Derian Escalante <deriannavy@gmail.com>
2023-06-13 19:43:42 -0600

    main banner adjunt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,10 @@
       <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
       <h1 class="display-5 fw-bold text-body-emphasis">Diez.dev</h1>
       <div class="col-lg-6 mx-auto">
-         <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+         <p class="lead mb-4">
+            This is un example of all the projects
+         <!-- Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins. -->
+         </p>
          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <button type="button" class="btn btn-primary btn px-4 gap-3">
                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-github" viewBox="0 0 20 20">
