c9ccd6fbc744a48fd126a5dea4c26bab6669b5b2
Derian Escalante <deriannavy@gmail.com>
2023-06-17 19:21:22 -0600

    main color schema

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,15 @@
   <RouterView/>
 </template>
 
-<script setup>
+<script>
 import { RouterView } from 'vue-router'
 import Header from '@/components/Header.vue'
+
+export default {
+  mounted(){
+    const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+    // recommended method for newer browsers: specify event-type as first argument
+    darkModePreference.addEventListener("change", e => document.body.setAttribute('data-bs-theme', e.matches ? 'dark' : 'light') );
+  }
+}
 </script>
