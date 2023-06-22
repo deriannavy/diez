9a855b569c1d8e0d0b5149ec666e4eff5e9ca415
Derian Escalante <deriannavy@gmail.com>
2023-06-21 19:32:49 -0600

    main ajustes link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -59,23 +59,29 @@
                               'background-position': 'center',
                               'background-size': 'contain',
                               'background-repeat': 'no-repeat',
-                              'margin-right': '20px'
+                              'margin-right': '10px'
                            }">
 
                      </div>
                   </div>
                </div>
-               <div class="card-footer text-body-secondary p-0 text-end">
+               <div class="card-footer p-0 d-flex flex-row justify-content-end">
 
-                  <router-link v-if="card.typeUrl == 'internal'" :to="card.url" class=" btn text-primary">
+                  <router-link v-if="card.typeUrl == 'internal'" :to="card.url" 
+                               class="d-flex flex-row align-items-center btn text-primary">
+
                      <span class="me-1">view case study</span>
                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
                      </svg>
+
                   </router-link>
 
                   <a v-else-if="card.typeUrl == 'external'"
-                     :href="card.url">
+                     :href="card.url"
+                     target="_blank" 
+                     class="d-flex flex-row align-items-center align-items-end btn text-primary">
+                     
                      <span class="me-1">view case study</span>
                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
