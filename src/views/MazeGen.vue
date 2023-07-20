f7fa9ebafbfddfc5e7d5e9d96e620fb25ec81ea6
Derian Escalante <deriannavy@gmail.com>
2023-07-19 21:24:40 -0600

    develop maze adjust

--- src/views/MazeGen.vue
+++ src/views/MazeGen.vue
@@ -5,6 +5,30 @@
 // Libs
 import p5 from 'p5';
 </script>
+<script>
+export default{
+	mounted(){
+		this.dtP5Instance = new p5(this.fnCanvasInitializer, this.$refs.canvas);
+	},
+	data(){
+		return {
+			dtP5Instance: null,
+			dtP5Canvas: null
+		}
+	},
+	methods:{
+		fnCanvasInitializer(sketch){
+
+			this.dtP5Canvas = sketch;
+
+			sketch.setup = this.fnCanvasSetUp;
+			sketch.draw = this.fnCanvasDraw;
+
+			
+		}
+	}
+}
+</script>
 
 <style type="text/css">
 
