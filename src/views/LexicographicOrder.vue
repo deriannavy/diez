53510fc5c3c1eda0487921065da8d8595c14b28e
Derian Escalante <deriannavy@gmail.com>
2023-08-21 21:28:07 -0600

    lexicographic-order ajuste

--- src/views/LexicographicOrder.vue
+++ src/views/LexicographicOrder.vue
@@ -5,13 +5,11 @@
 <script setup>
 // Libs
 import p5 from 'p5';
-// Classes
-import MazeGenCell from '@/classes/MazeGenCell.js';
 </script>
 <script>
 export default{
 	mounted(){
-		// this.$refs.canvas
+
 		this.dtP5Instance = new p5(this.fnCanvasInitializer, this.$refs.canvas);
 	},
 }
