0971435367cfb3ab54261179beae03e7eee2d75d
Derian Escalante <deriannavy@gmail.com>
2023-08-23 21:39:09 -0600

    lexicographic-order swapper

--- src/views/LexicographicOrder.vue
+++ src/views/LexicographicOrder.vue
@@ -38,43 +38,49 @@ export default{
 
 		},
 		fnCanvasDraw(){
-			console.log(vals);
+			// console.log(vals);
 
-    var largestI = -1;
-    for (let i = 0; i < vals.length; i++){
-        
-        if (vals[i] < vals[i + 1]) largestI = i;    
-    } 
-    
+		    // var largestI = -1;
+		    // for (let i = 0; i < vals.length; i++){
+		        
+		    //     if (vals[i] < vals[i + 1]) largestI = i;    
+		    // } 
+		    
 
-    if (largestI == -1) {
-        noLoop();
-        console.log('finished');
-    }
+		    // if (largestI == -1) {
+		    //     noLoop();
+		    //     console.log('finished');
+		    // }
 
-    var largestJ = -1;
-    for (let j = 0; j < vals.length; j++) {
-        
-        if (vals[largestI] < vals[j]) largestJ = j;    
-    }
-
-
-    swap(vals, largestI, largestJ);
+		    // var largestJ = -1;
+		    // for (let j = 0; j < vals.length; j++) {
+		        
+		    //     if (vals[largestI] < vals[j]) largestJ = j;    
+		    // }
 
 
+		    // swap(vals, largestI, largestJ);
 
-    let endArray = vals.splice(largestI + 1);
-    endArray.reverse();
-    vals = vals.concat(endArray);
 
-    background(0);
-    textSize(64);
-    var s = vals.join('');
 
-    fill(255);
+		    // let endArray = vals.splice(largestI + 1);
+		    // endArray.reverse();
+		    // vals = vals.concat(endArray);
 
-    text(s, 20, height / 2)
+		    // background(0);
+		    // textSize(64);
+		    // var s = vals.join('');
+
+		    // fill(255);
+
+		    // text(s, 20, height / 2)
+		},
+		fnSwap(a, i, j){
+		    var temp = a[i];
+		    a[i] = a[j],
+		    a[j] = temp;
 		}
 	}
+
 }
 </script>
