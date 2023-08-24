3e1e50b675ae86169cd5047e49eefd0ca59d4e7d
Derian Escalante <deriannavy@gmail.com>
2023-08-23 21:35:20 -0600

    lexicographic-order drawer

--- src/views/LexicographicOrder.vue
+++ src/views/LexicographicOrder.vue
@@ -27,8 +27,54 @@ export default{
 
 			
 		},
-		fnCanvasSetUp(){},
-		fnCanvasDraw(){}
+		fnCanvasSetUp(){
+			let containterWidth =  this.$refs.container.clientWidth,
+				 computedStyle = getComputedStyle(this.$refs.container);
+				 
+			let canvasWidth = containterWidth -= parseFloat(computedStyle.paddingLeft) + parseFloat(computedStyle.paddingRight),
+				canvasHeight = window.innerHeight / 2;
+
+			this.dtP5Canvas.createCanvas( canvasWidth,  canvasHeight);
+
+		},
+		fnCanvasDraw(){
+			console.log(vals);
+
+    var largestI = -1;
+    for (let i = 0; i < vals.length; i++){
+        
+        if (vals[i] < vals[i + 1]) largestI = i;    
+    } 
+    
+
+    if (largestI == -1) {
+        noLoop();
+        console.log('finished');
+    }
+
+    var largestJ = -1;
+    for (let j = 0; j < vals.length; j++) {
+        
+        if (vals[largestI] < vals[j]) largestJ = j;    
+    }
+
+
+    swap(vals, largestI, largestJ);
+
+
+
+    let endArray = vals.splice(largestI + 1);
+    endArray.reverse();
+    vals = vals.concat(endArray);
+
+    background(0);
+    textSize(64);
+    var s = vals.join('');
+
+    fill(255);
+
+    text(s, 20, height / 2)
+		}
 	}
 }
 </script>
