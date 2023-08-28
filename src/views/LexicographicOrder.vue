df4074e4ccf46498856bada1a0223597f4da2e45
Derian Escalante <deriannavy@gmail.com>
2023-08-27 20:15:54 -0600

    lexicographic-order avances

--- src/views/LexicographicOrder.vue
+++ src/views/LexicographicOrder.vue
@@ -1,5 +1,26 @@
 <template>
-	<h1>lexicograpich</h1>
+	<main ref="container" class="container col-xxl-10 px-4 py-5">
+
+		<section class="row d-flex align-items-center">
+			<div class="col col-12 col-md-6">
+				<router-link to="/" class="link-secondary mb-2 d-flex flex-row align-items-center">
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left me-2" 
+						  viewBox="0 0 16 16">
+					  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+					</svg>
+					<span>Case studies</span>
+				</router-link>
+
+				<h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Lexicographic order</h1>	
+				
+				<p class="col col-12">...</p>
+
+			</div>			
+		</section>
+
+
+		<section class="col-12" ref="canvas"></section>
+	</main>
 </template>
 
 <script setup>
@@ -14,7 +35,7 @@ export default{
 	},
 	data(){
 		return {
-			dtP5Canvas,
+			dtP5Canvas: null,
 			dtVals: [0, 1, 2, 3, 4, 5, 6]
 		}
 	},
